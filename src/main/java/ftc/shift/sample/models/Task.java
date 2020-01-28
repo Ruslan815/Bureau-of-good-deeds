ada95accaa3115bf60aa8450b337f7c71b0ddf55
Ruslan815 <ruslansaf2000@gmail.com>
2020-01-28 17:45:34 +0700

    Created interface of Task.java class

---
+++ src/main/java/ftc/shift/sample/models/Task.java
@@ -0,0 +1,28 @@
+package ftc.shift.sample.models;
+
+import java.util.List;
+
+public class Task {
+
+    private String taskId;
+
+    private String ownerId;
+
+    private String performerId;
+
+    private String taskName;
+
+    private Integer taskStatus;
+
+    private String taskDescription;
+
+    private Integer taskCost;
+
+    private String creationDate;
+
+    private String completionDate;
+
+    private String taskPicture;
+
+}
+
