49ad21f8a3bf54fdca297503e675b00b342007e4
Ruslan815 <ruslansaf2000@gmail.com>
2020-01-28 18:22:40 +0700

    Added setters and getters for class Task

--- src/main/java/ftc/shift/sample/models/Task.java
+++ src/main/java/ftc/shift/sample/models/Task.java
@@ -1,7 +1,5 @@
 package ftc.shift.sample.models;
 
-import java.util.List;
-
 public class Task {
 
     private String taskId;
@@ -23,6 +21,103 @@ public class Task {
     private String completionDate;
 
     private String taskPicture;
+
+    public Task() {
+    }
+
+    public Task(String taskId, String ownerId, String performerId, String taskName, Integer taskStatus, String taskDescription, Integer taskCost,
+                String creationDate, String completionDate, String taskPicture) {
+        this.taskId = taskId;
+        this.ownerId = ownerId;
+        this.performerId = performerId;
+        this.taskName = taskName;
+        this.taskStatus = taskStatus;
+        this.taskDescription = taskDescription;
+        this.taskCost = taskCost;
+        this.creationDate = creationDate;
+        this.completionDate = completionDate;
+        this.taskPicture = taskPicture;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getPerformerId() {
+        return performerId;
+    }
+
+    public void setPerformerId(String performerId) {
+        this.performerId = performerId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Integer getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(Integer taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public Integer getTaskCost() {
+        return taskCost;
+    }
+
+    public void setTaskCost(Integer taskCost) {
+        this.taskCost = taskCost;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(String completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    public String getTaskPicture() {
+        return taskPicture;
+    }
+
+    public void setTaskPicture(String taskPicture) {
+        this.taskPicture = taskPicture;
+    }
 
 }
 
